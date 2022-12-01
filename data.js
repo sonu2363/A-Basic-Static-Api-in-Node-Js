c9507fad61a77366018c1e9f962e13a6921b032f
Sonu Sah <sonusah2363@gmail.com>
2022-12-01 16:14:09 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:'Sonu Sah',email:'sonusah23012000@gmail.com'},
+    {name:'Sam Sah',email:'son@gmail.com'},
+    {name:'Peter Sah',email:'23012000@gmail.com'}
+]
+
+module.exports=data;
